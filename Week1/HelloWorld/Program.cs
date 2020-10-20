9fbbf5eb03c74fbf927cdd1ff96287e617bf07da
Shalei Kumar <shaleik@bu.edu>
2020-10-20 09:57:07 -0400

    First HelloWorld Project Commit

---
+++ Week1/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿//1.0 predefined namespace
+using System;
+
+//1.1 custom defined namespace
+namespace HelloWorld
+{
+    class Program
+    {
+        // execution starts here
+                        //Commandline arguements
+                        // vvv
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Welcome {name}");
+
+        }
+    }
+}
